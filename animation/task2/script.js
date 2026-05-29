a92a0ffb9ed92fc6b1317dbf7f2d792d3bdb9d37
Arjabi04 <arjabishrestha@gmail.com>
2026-05-29 18:34:31 +0545

    scroll animation task2

---
+++ animation/task2/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Create 12 boxes in each row
+    document.querySelectorAll(".image-animation__row").forEach(row => {
+
+        for (let i = 0; i < 12; i++) {
+
+            const box = document.createElement("div");
+
+            box.classList.add("image-animation__item");
+
+            row.appendChild(box);
+        }
+
+    });
+
+    // take each row and set a delay based on its index
+document.querySelectorAll(".image-animation").forEach(section => {
+    section.querySelectorAll(".image-animation__row").forEach((row, index) => {
+        row.style.setProperty("--delay", `${index * 0.3}s`);
+    });
+});
+
+    // Scroll animations
+    const observer = new IntersectionObserver((entries, observer) => {
+
+        entries.forEach(entry => {
+    console.log(entry.target, entry.isIntersecting, entry.intersectionRatio);
+
+    if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+    }
+});
+
+    }, {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0
+    });
+
+    document
+    .querySelectorAll(".image-animation__row, .text")
+    .forEach(element => observer.observe(element));
+
+});
